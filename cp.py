2556fa8d8e58738ece7a3f23e3de1a0b86b2affa
Jaisaideep <112754005+Jaisaideep@users.noreply.github.com>
2024-04-05 16:00:46 +0530

    Update cp.py

--- cp.py
+++ cp.py
@@ -3,52 +3,59 @@ import json
 import array
 import pandas as pd
 import numpy as np
+from google.cloud import bigquery
 from datetime import datetime, timedelta
 
-d = datetime.utcnow() - timedelta(hours=1)
+def terradata(event,context):
+    d = datetime.utcnow() - timedelta(hours=1)
+    StartTime = d.strftime('%Y-%m-%dT%H:00:0000000Z')
+    EndTime = datetime.utcnow().strftime('%Y-%m-%dT%H:00:0000000Z')
+    endpoint = "http://io.catchpoint.com/api/v2/tests/explorer/favoritechart/data/25334"
+    # parameters = {"startTimeUtc": {StartTime}, "endTimeUtc": {EndTime}}
+    parameters = {"start": {StartTime}, "end": {EndTime}}
+    headers = {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer AD9E312743A1DE9278FBB05BB3D2057AAA9A5839E625D8BB823B59C7EC7F2A7E"}
+    #http_proxy = "http://proxy.ebiz.verizon.com:80"
+    #proxies = {"http": http_proxy}
+    JsonData = requests.get(endpoint, params=parameters, headers=headers).json()
+    # Manually loaded the JSON result from postman to a file to parse the JSON values;
+    # This can be removed and  JSONData can be passed to JsonSampleData
 
-StartTime = d.strftime('%Y-%m-%dT%H:00:0000000Z')
-EndTime = datetime.utcnow().strftime('%Y-%m-%dT%H:00:0000000Z')
-print (StartTime)
-print (EndTime)
+    # data = json.load(ResponseJson)
 
-endpoint = "http://io.catchpoint.com/api/v2/tests/explorer/favoritechart/data/34649"
-parameters = {"start": {StartTime}, "end": {EndTime}}
-headers = {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer AD9E312743A1DE9278FBB05BB3D2057AAA9A5839E625D8BB823B59C7EC7F2A7E"}
+    # Declaring arrays
+    Loggedtime = []
+    AppName = []
+    MetricsName = []
+    MetricsVal = []
+    # code to get the metrics name by Traversing the JSON
+    for i in JsonData['data']['favoriteCharts'][0]['summary']['fields']['syntheticMetrics']:
+        MetricsName.append(i['name'])
+    # Code to get other values like Time, AppName and Test results
+    for i in JsonData['data']['favoriteCharts'][0]['detail']['items']:
 
-JsonData = requests.get(endpoint, params=parameters, headers=headers).json()
+        Loggedtime.append(i['dateTime'])
+        AppName.append(i['dimension'][1]['name'])
+        MetricsVal.append(i['syntheticMetrics'])
 
-# Declaring arrays
-Loggedtime = []
-AppName = []
-MetricsName = []
-MetricsVal = []
+    # Creating a Dataframe to Load the data in tabular format, this can be pushed to BQ
 
-# code to get the metrics name by Traversing the JSON
-for i in JsonData['data']['favoriteCharts'][0]['summary']['fields']['syntheticMetrics']:
-    MetricsName.append(i['name'])
-print (MetricsName)
+    df = pd.DataFrame(data=np.array(MetricsVal), columns=np.array(MetricsName))
+    df['AppName'] = AppName
+    df['LogTime'] = Loggedtime
 
-# Code to get other values like Time, AppName and Test results
-for i in JsonData['data']['favoriteCharts'][0]['detail']['items']:
-    Loggedtime.append(i['dateTime'])
-    AppName.append(i['dimension'][1]['name'])
-    MetricsVal.append(i['syntheticMetrics'])
-
-df = pd.DataFrame(data=np.array(MetricsVal), columns=np.array(MetricsName))
-df['AppName'] = AppName
-df['LogTime'] = Loggedtime
-print(df)
-
-#Rearrange the order of df
-NewDF = df[['AppName','LogTime','TestTime','RequestTestResponseTime','WaitTime','SyntheticExperienceScore','AvailabilityPercent']]
-
-
-CDP Endpoint = http://io.catchpoint.com/api/v2/tests/explorer/favoritechart/data/34648
-VZOS Endpoint = http://io.catchpoint.com/api/v2/tests/explorer/favoritechart/data/34646
-Markeplace Endpoint = http://io.catchpoint.com/api/v2/tests/explorer/favoritechart/data/34643
-REDIS EndPoint = http://io.catchpoint.com/api/v2/tests/explorer/favoritechart/data/35503
-SMART DQ  EndPoint = http://io.catchpoint.com/api/v2/tests/explorer/favoritechart/data/35504
-Mark Logic Endpoint = http://io.catchpoint.com/api/v2/tests/explorer/favoritechart/data/35581
+    print(df)
+    table_id = 'AIDSRE.DATAROBOT_CP_DATA'
+    Client = bigquery.Client()
+   # table_id1 = 'AIDSRE.TD_LOWVERSIONDRIVERS'
+    # Since string columns use the "object" dtype, pass in a (partial) schema
+    # to ensure the correct BigQuery data type.
+    job_config = bigquery.LoadJobConfig(
+        write_disposition=bigquery.job.WriteDisposition.WRITE_APPEND
+    )
+    
+    job = Client.load_table_from_dataframe(
+                        df, table_id,job_config=job_config
+                            )
+    job.result()
