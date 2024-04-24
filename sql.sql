02ec3bcb893d01c527e1daadf6801e3ff0322973
Jaisaideep <112754005+Jaisaideep@users.noreply.github.com>
2024-04-24 18:49:40 +0530

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,18 +1,21 @@
-SELECT date(t1.Timestamp) as LogDate,
-#Application,
-#avg(CPU_Usage) as AVG_CPU_Usge,
-(CASE
-   WHEN t1.Application = 'CDP' THEN 0.1*(avg(t1.CPU_Usage))
-   WHEN t1.Application = 'DataMarketplace' THEN 0.25*(avg(t1.CPU_Usage))
-   WHEN t1.Application = 'VZOS' THEN 0.1*(avg(t1.CPU_Usage))
-   WHEN t1.Application = 'RedisLabs' THEN 0.05 *(avg(t1.CPU_Usage))
-   WHEN t1.Application = 'ML' THEN 0.1*(avg(t1.CPU_Usage))
-   WHEN t1.Application = 'SmartDQ' THEN 0.1*(avg(t1.CPU_Usage))
-   WHEN t1.Application = 'MDE' THEN 0.05*(avg(t1.CPU_Usage))
-  ELSE 0
-END) AS AggAvg
-FROM `vz-it-pr-jabv-aidplt-0.AIDSRE.infra_dgs` as t1
-where date(t1.Timestamp) = '2024-04-22'
-GROUP BY 1
-
-SELECT list expression references t1.Application which is neither grouped nor aggregated at [8:9]
+SELECT 
+  DATE(t1.Timestamp) AS LogDate,
+  AVG(t1.CPU_Usage) AS AVG_CPU_Usage,
+  SUM(
+    CASE
+      WHEN t1.Application = 'CDP' THEN 0.1 * t1.CPU_Usage
+      WHEN t1.Application = 'DataMarketplace' THEN 0.25 * t1.CPU_Usage
+      WHEN t1.Application = 'VZOS' THEN 0.1 * t1.CPU_Usage
+      WHEN t1.Application = 'RedisLabs' THEN 0.05 * t1.CPU_Usage
+      WHEN t1.Application = 'ML' THEN 0.1 * t1.CPU_Usage
+      WHEN t1.Application = 'SmartDQ' THEN 0.1 * t1.CPU_Usage
+      WHEN t1.Application = 'MDE' THEN 0.05 * t1.CPU_Usage
+      ELSE 0
+    END
+  ) AS AggAvg
+FROM 
+  `vz-it-pr-jabv-aidplt-0.AIDSRE.infra_dgs` AS t1
+WHERE 
+  DATE(t1.Timestamp) = '2024-04-22'
+GROUP BY 
+  LogDate
