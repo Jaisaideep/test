9bb2c30394fbdd2b0fe2bd95b45a980b877b40f5
Jaisaideep <112754005+Jaisaideep@users.noreply.github.com>
2024-04-26 00:41:35 +0530

    Update CF.py

--- CF.py
+++ CF.py
@@ -1,61 +1,112 @@
 import requests
 import json
-import array
 import pandas as pd
 import numpy as np
-from google.cloud import bigquery
 from datetime import datetime, timedelta
+from pandas_gbq import to_gbq
+from google.cloud import bigquery
+from google.api_core.exceptions import NotFound
 
-def terradata(event,context):
-    d = datetime.utcnow() - timedelta(hours=1)
-    StartTime = d.strftime('%Y-%m-%dT%H:00:0000000Z')
-    EndTime = datetime.utcnow().strftime('%Y-%m-%dT%H:00:0000000Z')
-    endpoint = "http://io.catchpoint.com/api/v2/tests/explorer/favoritechart/data/25334"
-    # parameters = {"startTimeUtc": {StartTime}, "endTimeUtc": {EndTime}}
-    parameters = {"start": {StartTime}, "end": {EndTime}}
+def fetch_and_store_data(event):
+    """
+    Fetches data from multiple endpoints, consolidates it into a dataframe,
+    and stores it in a BigQuery table.
+    Args:
+        event: The event data passed to the function. Unused in this function.
+    Returns:
+        str: A message indicating the completion of data extraction and storage.
+    """
+    # Set parameters and headers for the request
     headers = {
-                "Content-Type": "application/json",
-                "Authorization": "Bearer AD9E312743A1DE9278FBB05BB3D2057AAA9A5839E625D8BB823B59C7EC7F2A7E"}
-    #http_proxy = "http://proxy.ebiz.verizon.com:80"
-    #proxies = {"http": http_proxy}
-    JsonData = requests.get(endpoint, params=parameters, headers=headers).json()
-    # Manually loaded the JSON result from postman to a file to parse the JSON values;
-    # This can be removed and  JSONData can be passed to JsonSampleData
+        "Content-Type": "application/json",
+        "Authorization": "Bearer YOUR_ACCESS_TOKEN"
+    }
 
-    # data = json.load(ResponseJson)
+    # List of endpoints to fetch data from
+    endpoints = [
+        "http://io.catchpoint.com/api/v2/tests/explorer/favoritechart/data/34649",
+        "http://io.catchpoint.com/api/v2/tests/explorer/favoritechart/data/34648",
+        "http://io.catchpoint.com/api/v2/tests/explorer/favoritechart/data/34646",
+        "http://io.catchpoint.com/api/v2/tests/explorer/favoritechart/data/34643",
+        "http://io.catchpoint.com/api/v2/tests/explorer/favoritechart/data/35503",
+        "http://io.catchpoint.com/api/v2/tests/explorer/favoritechart/data/35504",
+        "http://io.catchpoint.com/api/v2/tests/explorer/favoritechart/data/35581"
+    ]
 
-    # Declaring arrays
-    Loggedtime = []
-    AppName = []
-    MetricsName = []
-    MetricsVal = []
-    # code to get the metrics name by Traversing the JSON
-    for i in JsonData['data']['favoriteCharts'][0]['summary']['fields']['syntheticMetrics']:
-        MetricsName.append(i['name'])
-    # Code to get other values like Time, AppName and Test results
-    for i in JsonData['data']['favoriteCharts'][0]['detail']['items']:
+    # Initialize an empty list to store dataframes
+    dfs = []
 
-        Loggedtime.append(i['dateTime'])
-        AppName.append(i['dimension'][1]['name'])
-        MetricsVal.append(i['syntheticMetrics'])
+    # Iterate over each endpoint to fetch data
+    for endpoint in endpoints:
+        # Make the API request
+        JsonData = requests.get(endpoint, headers=headers).json()
 
-    # Creating a Dataframe to Load the data in tabular format, this can be pushed to BQ
+        # Extracting data from the JSON response
+        Loggedtime = []
+        AppName = []
+        MetricHeader = []
+        MetricData = []
 
-    df = pd.DataFrame(data=np.array(MetricsVal), columns=np.array(MetricsName))
-    df['AppName'] = AppName
-    df['LogTime'] = Loggedtime
+        # Extracting metrics names
+        for i in JsonData['data']['favoriteCharts'][0]['summary']['fields']['syntheticMetrics']:
+            MetricHeader.append(i['name'])
 
-    print(df)
-    table_id = 'AIDSRE.DATAROBOT_CP_DATA'
-    Client = bigquery.Client()
-   # table_id1 = 'AIDSRE.TD_LOWVERSIONDRIVERS'
-    # Since string columns use the "object" dtype, pass in a (partial) schema
-    # to ensure the correct BigQuery data type.
-    job_config = bigquery.LoadJobConfig(
-        write_disposition=bigquery.job.WriteDisposition.WRITE_APPEND
-    )
-    
-    job = Client.load_table_from_dataframe(
-                        df, table_id,job_config=job_config
-                            )
-    job.result()
+        # Extracting metrics values, logged time, and app name
+        for i in JsonData['data']['favoriteCharts'][0]['detail']['items']:
+            Loggedtime.append(i['dateTime'])
+            AppName.append(i['dimension'][1]['name'])
+            MetricData.append(i['syntheticMetrics'])
+
+        # Creating a dataframe from the extracted data
+        df = pd.DataFrame(data=np.array(MetricData), columns=np.array(MetricHeader))
+        df['AppName'] = AppName
+        df['LogTime'] = pd.to_datetime(Loggedtime)  # Convert 'LogTime' to datetime
+
+        # Append the dataframe to the list
+        dfs.append(df)
+
+    # Concatenate all dataframes into one
+    result_df = pd.concat(dfs, ignore_index=True)
+
+    # Reordering the columns
+    result_df = result_df[['AppName','LogTime','TestTime','RequestTestResponseTime','WaitTime','SyntheticExperienceScore','AvailabilityPercent']]
+
+    # Check if the table is empty
+    table_empty = True
+
+    # Query BigQuery to check if the table is empty
+    client = bigquery.Client()
+    table_ref = client.dataset("AIDSRE").table("SRE_DA_Prod_Reliability_Ingress_CP")
+
+    try:
+        table = client.get_table(table_ref)
+        if table.num_rows > 0:
+            table_empty = False
+    except NotFound:
+        pass # Table not found
+
+    # Define the schema for the BQ Table
+    schema = [
+        {"name":"AppName", "type":"STRING"},
+        {"name":"LogTime", "type":"TIMESTAMP"},
+        {"name":"TestTime", "type":"FLOAT64"},
+        {"name":"RequestTestResponseTime", "type":"FLOAT64"},
+        {"name":"WaitTime", "type":"FLOAT64"},
+        {"name":"SyntheticExperienceScore", "type":"FLOAT64"},
+        {"name":"AvailabilityPercent", "type":"FLOAT64"}
+    ]
+
+    # Define BQ Table Details
+    GCP_Project_ID = "YOUR_PROJECT_ID"
+    Table_Name = "AIDSRE.SRE_DA_Prod_Reliability_Ingress_CP"
+
+    # Inject Data to BQ
+    if table_empty:
+        to_gbq(result_df, destination_table=Table_Name, project_id=GCP_Project_ID, if_exists="replace", table_schema=schema)
+    else:
+        # Filter records in newDF based on timestamps
+        max_timestamp = result_df['LogTime'].max()
+        new_records = result_df[result_df['LogTime'] > max_timestamp]
+        to_gbq(new_records, destination_table=Table_Name, project_id=GCP_Project_ID, if_exists="append", table_schema=schema)
+
+    return 'Data has been extracted and stored in BigQuery table.'
