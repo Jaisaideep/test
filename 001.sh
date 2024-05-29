1028a2b7882849eb0559b363b58c73498bf85000
Jaisaideep <112754005+Jaisaideep@users.noreply.github.com>
2024-05-29 18:26:44 +0530

    Update 001.sh

--- 001.sh
+++ 001.sh
@@ -3,8 +3,8 @@
 cd /apps/opt/application/looker
 # set your java memory- there should be over 1.5G of system memory 
 # left to run the OS
-MEM=`cat /proc/meminfo | grep MemTotal | awk '{print $2}'`
-JM=`expr $MEM \* 6 / 10`
+MEM=$(cat /proc/meminfo | grep MemTotal | awk '{print $2}')
+JM=$(expr $MEM \* 6 / 10)
 JAVAMEM="46000m"
 METAMEM="1000m"
 
@@ -29,9 +29,9 @@ export LKR_MASTER_KEY_FILE=/home/looker/looker/looker_key.txt
 echo "${LOOKERARGS}" | grep -q "\-\-no\-ssl"
 if [ $? -eq 0 ] 
 then
-	PROTOCOL='http'
+        PROTOCOL='http'
 else
-	PROTOCOL='https'
+        PROTOCOL='https'
 fi
 LOOKERPORT=${LOOKERPORT:-"9999"}
 
@@ -46,7 +46,7 @@ start() {
         echo "Startup suppressed: ${LOCKFILE} contains running pid, startup script is already running"
         exit 1
     fi
-    
+
     # make sure the lockfile is removed when we exit and then claim it
     trap "rm -f ${LOCKFILE}; exit" INT TERM EXIT
     echo $$ > ${LOCKFILE}
@@ -67,68 +67,77 @@ start() {
 }
 
 stop() {
-    pid=`cat .tmp/looker.pid`
-    if [ -f .status_server_token ] && [ -x /usr/bin/curl ]; then
-        state="running"
-        token=`cat .status_server_token`
-        request="control/stop?token=${token}"
-        timeout 20 curl -m 10 -ks ${PROTOCOL}://127.0.0.1:${LOOKERPORT}/${request} > /dev/null 2>&1
-        ECODE=$?
-        [ $ECODE -eq 7 ] && state="stopped"
-        if [ $ECODE -gt 7 ] ; then
-            kill $pid
-        fi
-        for i in {1..30}; do
-            timeout 20 curl -m 5 -ks ${PROTOCOL}://127.0.0.1:${LOOKERPORT}/alive > /dev/null 2>&1
-            ECODE=$?
-            if [ $ECODE -eq 7 ]; then
-              state="stopped"
-              break
+    if [ -e .tmp/looker.pid ]; then
+        pid=$(cat .tmp/looker.pid)
+        if ps -p $pid > /dev/null; then
+            # Process exists, so attempt to stop it
+            if [ -f .status_server_token ] && [ -x /usr/bin/curl ]; then
+                state="running"
+                token=$(cat .status_server_token)
+                request="control/stop?token=${token}"
+                timeout 20 curl -m 10 -ks ${PROTOCOL}://127.0.0.1:${LOOKERPORT}/${request} > /dev/null 2>&1
+                ECODE=$?
+                [ $ECODE -eq 7 ] && state="stopped"
+                if [ $ECODE -gt 7 ] ; then
+                    kill $pid
+                fi
+                for i in {1..30}; do
+                    timeout 20 curl -m 5 -ks ${PROTOCOL}://127.0.0.1:${LOOKERPORT}/alive > /dev/null 2>&1
+                    ECODE=$?
+                    if [ $ECODE -eq 7 ]; then
+                        state="stopped"
+                        break
+                    fi
+                    if [ $ECODE -gt 7 ] ; then
+                        kill -9 $pid
+                    fi
+                    sleep 1
+                done
+                if [ "${state}" = "running" ]; then
+                    echo "Force Stop Looker Web Application"
+                    kill $pid
+                    kill -0 $pid && kill -9 $pid
+                fi
+            else
+                timeout 20 java -jar looker.jar stop
+                if [ $? -ne 0 ]; then
+                    kill -9 $pid
+                fi
             fi
-            if [ $ECODE -gt 7 ] ; then
-                kill -9 $pid
-            fi
-            sleep 1
-        done
-        if [ "${state}" = "running" ]; then
-            echo "Force Stop Looker Web Application"
-            kill $pid
-            kill -0 $pid && kill -9 $pid
+        else
+            echo "Looker process with PID $pid is not running."
         fi
     else
-        timeout 20 java -jar looker.jar stop
-        if [ $? -ne 0 ]; then
-            kill -9 $pid
-        fi
+        echo "Looker PID file does not exist."
     fi
 }
 
 fixcrypt() {
-    CRYPTEXIST=`/sbin/ldconfig -p | grep -c '\slibcrypt.so\s'`
+    CRYPTEXIST=$(/sbin/ldconfig -p | grep -c '\slibcrypt.so\s')
 
     if [ $CRYPTEXIST -eq 0 ]; then
         if [ ! -d .tmp ]; then
             mkdir .tmp
         fi
-        CRYPTLN=`/sbin/ldconfig -p | grep '\slibcrypt\.so\.[[:digit:]]' | awk '{print $(NF)}'`
-        ln -s -f $CRYPTLN `pwd`/.tmp/libcrypt.so
-        export LD_LIBRARY_PATH=`pwd`/.tmp/:$LD_LIBRARY_PATH
+        CRYPTLN=$(/sbin/ldconfig -p | grep '\slibcrypt\.so\.[[:digit:]]' | awk '{print $(NF)}')
+        ln -s -f $CRYPTLN $(pwd)/.tmp/libcrypt.so
+        export LD_LIBRARY_PATH=$(pwd)/.tmp/:$LD_LIBRARY_PATH
     fi
 }
 
 case "$1" in
   start)
     start
-	;;
+        ;;
   stop)
     stop
-	;;
+        ;;
   restart)
-	echo "Restarting Looker Web Application" "looker"
+        echo "Restarting Looker Web Application" "looker"
         stop
         sleep 3
         start
-	;;
+        ;;
   status)
         curl -ks ${PROTOCOL}://127.0.0.1:${LOOKERPORT}/alive > /dev/null 2>&1
         if [ $? -eq 7 ]; then
